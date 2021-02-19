b98ae34cfa0bfee623d39ff66c849ae67d4c704a
Andreas Edmeier <andreas.edmeier@web.de>
2021-02-19 20:43:40 +0100

    added ChangeDataStrategyComposit

---
+++ Apollo/Assets/Code/StructureClasses/ChangeDataStrategyComposit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Apollo
+{
+	[CreateAssetMenu(fileName = "DataStrategy_Composit", menuName = "Game/ChangeDataStrategy/Composit")]
+	public class ChangeDataStrategyComposit : ChangeDataStrategy
+	{
+		[SerializeField] ChangeDataStrategy[] myStrategys;
+
+		public override void DoChange() {
+			foreach(var it in myStrategys) {
+				it.DoChange();
+			}
+		}
+	}
+}
