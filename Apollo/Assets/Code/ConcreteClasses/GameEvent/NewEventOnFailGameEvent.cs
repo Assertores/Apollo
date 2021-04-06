ebd461e9f3dfc49dff9ea3767f68ebca4f0c7df8
Andreas Edmeier <andreas.edmeier@web.de>
2021-04-06 18:21:51 +0200

    add more flexible Game event

---
+++ Apollo/Assets/Code/ConcreteClasses/GameEvent/NewEventOnFailGameEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Apollo
+{
+	public class NewEventOnFailGameEvent : GameEvent
+	{
+		[Tooltip("the way this game event makes it self known to the players")]
+		[SerializeField] ChangeDataStrategy myStartDataChange;
+		[Tooltip("the effect this event has on the system if it succeads")]
+		[SerializeField] ChangeDataStrategy mySuccessDataChange;
+		[Tooltip("the effect this event has on the system if it fails")]
+		[SerializeField] ChangeDataStrategy myFailiarDataChange;
+		[Tooltip("gets triggered if this event failes")]
+		[SerializeField] GameEvent myFailiarEvent;
+
+		protected override void OnStart() {
+			myStartDataChange?.DoChange();
+		}
+
+		protected override void OnFinished(bool aSuccessfull) {
+			if(aSuccessfull) {
+				myStartDataChange?.DoChange();
+			} else {
+				myFailiarDataChange?.DoChange();
+				OnInstanciation(myFailiarEvent);
+			}
+		}
+	}
+}
