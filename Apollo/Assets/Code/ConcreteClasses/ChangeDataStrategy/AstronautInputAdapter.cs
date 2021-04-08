f26b86fe2e64033c6f22e29350de978bb92f16a3
Andreas Edmeier <andreas.edmeier@web.de>
2021-04-09 01:00:54 +0200

    create Command to Astronaut Input Adapter and Data

---
+++ Apollo/Assets/Code/ConcreteClasses/ChangeDataStrategy/AstronautInputAdapter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Apollo
+{
+	[CreateAssetMenu(fileName = "AIAdapts_Element", menuName = "Game/AstronautInput/Adapter")]
+	public class AstronautInputAdapter : Command
+	{
+		[Tooltip("the Astronaut Input that gets Run when this command runs")]
+		[SerializeField] AstronautInput myInput;
+
+		public override void Execute() {
+			AstronautInputBus.s_instance.RunInput(myInput);
+		}
+	}
+}
