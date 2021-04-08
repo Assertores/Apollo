a78b01624b91154626064ca7fa9b894f93c73547
Andreas Edmeier <andreas.edmeier@web.de>
2021-04-09 01:00:14 +0200

    create VR Button and ToggleSwitch

---
+++ Apollo/Assets/Code/Game/VR/AstronautButton.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Apollo
+{
+	public class AstronautButton : MonoBehaviour
+	{
+		[Tooltip("the actual Button whitch travels")]
+		[SerializeField] Rigidbody myButtonObject;
+		[Tooltip("at this depth the the button is considert as pressed")]
+		[SerializeField] GameObject myActiveDempth;
+		[Tooltip("this command gets executed when the button is pressed")]
+		[SerializeField] Command myOnPressed;
+		[Tooltip("this command gets executed when the button is released")]
+		[SerializeField] Command myOnReleased;
+
+		[Tooltip("the force with witch the button presses against the hand")]
+		[SerializeField] float myForce;
+		[Tooltip("wether the button should stay pressed and has to be pressed again to release or not")]
+		[SerializeField] bool myStayPressed = false;
+
+		float myMaxDistance;
+		float myActivateDistance;
+		enum State
+		{
+			Released,
+			ReadyToBePressed,
+			Pressed,
+			ReadyToBeReleased,
+		}
+		State myState = State.Released;
+
+		private void Start() {
+			myMaxDistance = -myButtonObject.transform.localPosition.z;
+			myActivateDistance = -myActiveDempth.transform.localPosition.z;
+			myButtonObject.constraints = RigidbodyConstraints.FreezeRotation;
+		}
+
+		private void Update() {
+			myButtonObject.transform.localPosition = Vector3.forward * myButtonObject.transform.localPosition.z;
+			var dist = -myButtonObject.transform.localPosition.z;
+
+			switch(myState) {
+			case State.Released:
+				if(dist < myActivateDistance) {
+					myOnPressed?.Execute();
+					myState = myStayPressed ? State.ReadyToBePressed : State.Pressed;
+				}
+				break;
+			case State.ReadyToBePressed:
+				break;
+			case State.Pressed:
+				break;
+			case State.ReadyToBeReleased:
+				if(dist > myActivateDistance) {
+					myOnReleased?.Execute();
+					myState = State.Released;
+				}
+				break;
+			default:
+				throw new System.InvalidCastException(myState.ToString());
+			}
+		}
+
+		private void FixedUpdate() {
+			myButtonObject.velocity = Vector3.zero;
+			myButtonObject.AddRelativeForce(-transform.forward * myForce, ForceMode.Force);
+
+			var dist = -myButtonObject.transform.localPosition.z;
+			var max = myStayPressed && (myState == State.ReadyToBePressed || myState == State.Pressed) ? myActivateDistance : myMaxDistance;
+			if(dist > max) {
+				myButtonObject.transform.localPosition = Vector3.forward * -max;
+				if(myState == State.ReadyToBePressed) {
+					myState = State.Pressed;
+				}
+				return;
+			}
+			if(myState == State.Pressed) {
+				myState = State.ReadyToBeReleased;
+			}
+			if(dist < 0) {
+				myButtonObject.transform.localPosition = Vector3.zero;
+			}
+		}
+	}
+}
---
+++ Apollo/Assets/Code/Game/VR/AstronautToggleSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Apollo
+{
+	public class AstronautToggleSwitch : MonoBehaviour
+	{
+		[Tooltip("the actual Button whitch travels")]
+		[SerializeField] Rigidbody mySwitchObject;
+		[Tooltip("this command gets executed when the button is pressed")]
+		[SerializeField] Command myOnActivated;
+		[Tooltip("this command gets executed when the button is released")]
+		[SerializeField] Command myOnDeactivated;
+
+		[Tooltip("the force with witch the button presses against the hand")]
+		[SerializeField] float myForce;
+
+		float myMaxAngle;
+		bool myIsActivated = false;
+
+		void Start() {
+			myMaxAngle = mySwitchObject.transform.localEulerAngles.x;
+			mySwitchObject.constraints = RigidbodyConstraints.FreezePosition;
+		}
+		
+		void Update() {
+			mySwitchObject.transform.localPosition = Vector3.zero;
+			var currentAngle = ((mySwitchObject.transform.localEulerAngles.x + 180) % 360) - 180;
+			mySwitchObject.transform.localEulerAngles = Vector3.right * currentAngle;
+
+			if(currentAngle > 0 && myIsActivated) {
+				myOnDeactivated?.Execute();
+				myIsActivated = false;
+			}
+			if(currentAngle < 0 && !myIsActivated) {
+				myOnActivated?.Execute();
+				myIsActivated = true;
+			}
+		}
+
+		private void FixedUpdate() {
+			var currentAngle = ((mySwitchObject.transform.localEulerAngles.x + 180) % 360) - 180;
+			mySwitchObject.angularVelocity = Vector3.zero;
+			mySwitchObject.AddRelativeTorque(Vector3.right * (currentAngle > 0 ? myForce : -myForce), ForceMode.Force);
+			
+			if(currentAngle > myMaxAngle) {
+				mySwitchObject.transform.localEulerAngles = Vector3.right * myMaxAngle;
+			}
+			if(currentAngle < -myMaxAngle) {
+				mySwitchObject.transform.localEulerAngles = Vector3.right * -myMaxAngle;
+			}
+		}
+	}
+}
